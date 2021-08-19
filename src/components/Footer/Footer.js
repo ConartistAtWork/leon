786c2a85589f42d74573ffc6d0a208bb493de7b1
ConartistAtWork <mitet101@gmail.com>
2021-08-19 17:57:21 +0300

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css"
 function Footer() {
   return (
     <footer>
-      <p>© 2019 - <span>Leon</span>,All Rights Reserved</p>
+      <p>© 2019 - <span>Leon</span>, All Rights Reserved</p>
     </footer>
   )
 }
